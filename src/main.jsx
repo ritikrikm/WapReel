c7358e234dcf390f3e20d03903a04dc2b49f6504
Ritik Mehta <rik@Ritiks-MacBook-Pro.local>
2025-11-09 20:43:18 -0500

    Trigger BJ

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <Provider>
+        <App />
+    </Provider>
 )
